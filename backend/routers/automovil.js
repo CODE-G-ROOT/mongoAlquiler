4460ed74a37ea44187e6b58e6e0d72cb29ebd3e5
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-08-16 13:14:17 -0500

    feat: :safety_vest: Middleware y DTO
    implementación de seguridad para las colecciónes

--- backend/routers/automovil.js
+++ backend/routers/automovil.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { confiGET } from "../limit/limit.js";
+import { middle_Verify_Automovil, DTO_data_automovil } from "../middleware/automovil.js";
 
 import { 
     getAuomoviles_Modelo_Marca,
@@ -12,9 +13,9 @@ import {
 const automovil = Router();
 
 automovil.get('/', confiGET() ,getAutomoviles);
-automovil.get('/disponibles', confiGET() ,getAutomoviles_Alquiler);
-automovil.get('/capacidad/qte=:id', confiGET(), getAuomoviles_QT);
-automovil.get('/capacidad/lte=:id', confiGET(), getAuomoviles_LT);
-automovil.get('/modelo_marca', confiGET(), getAuomoviles_Modelo_Marca);
+automovil.get('/disponibles', confiGET() ,getAutomoviles_Alquiler, middle_Verify_Automovil);
+automovil.get('/capacidad/qte=:id', confiGET(), getAuomoviles_QT, middle_Verify_Automovil);
+automovil.get('/capacidad/lte=:id', confiGET(), getAuomoviles_LT, middle_Verify_Automovil);
+automovil.get('/modelo_marca', confiGET(), getAuomoviles_Modelo_Marca, middle_Verify_Automovil);
 
 export default automovil;
