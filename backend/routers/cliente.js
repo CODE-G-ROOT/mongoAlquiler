e4da14923b3972c47ee4df4d664c42cb5cb89f43
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-08-16 13:18:17 -0500

    feat: :safety_vest: Middelware y DTO
    Configuración de las validaciones y seguridad

--- backend/routers/cliente.js
+++ backend/routers/cliente.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { confiGET } from "../limit/limit.js";
-
+import { middle_Verify_cliente, DTO_data_cliente } from "../middleware/cliente.js";
 import { 
     getClientes,
     getCliente_DNI,
@@ -11,8 +11,8 @@ import {
 const clientes = Router();
 
 clientes.get('/', confiGET(), getClientes);
-clientes.get('/dni=:id', confiGET(), getCliente_DNI);
-clientes.get('/alquiler', confiGET(), getCliente_Alquiler);
+clientes.get('/dni=:id', confiGET(), getCliente_DNI, middle_Verify_cliente);
+clientes.get('/alquiler', confiGET(), getCliente_Alquiler, middle_Verify_cliente);
 
 
 export default clientes
