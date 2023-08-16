016bcaaa6dfcdd721566ce2e475068569fee91a7
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-08-16 13:21:33 -0500

    feat: :safety_vest: Middleware y DTO
    Implmentación de seguridad para la coleccion de empleados

---
+++ backend/middleware/empleado.js
@@ -0,0 +1,43 @@
+import { plainToClass, classToPlain } from 'class-transformer';
+import { validate } from 'class-validator';
+import { DTO } from '../limit/token.js';
+import { Router } from 'express';
+import 'reflect-metadata';
+
+const middle_Verify_empleado = Router()
+const DTO_data_empleado = Router()
+
+middle_Verify_empleado.use((req, res, next)=>{
+    if(!req.rateLimit) return;
+
+    let { payload } = req.data;
+    const { ait, exp, ...newPayload } = payload;
+    payload = newPayload;
+
+    console.log({payload});
+
+    let Clone = JSON.stringify( classToPlain( plainToClass( DTO("empleado").class, {}, { ignoreDecorators: true})));
+    console.log(Clone);
+
+    let Verify = Clone === JSON.stringify(payload);
+    req.data = undefined; 
+
+    (!Verify) 
+        ? res.status(406).send({ status: 406, message: "Not autorized", reference: "https://http.cat/406"})
+        : next()
+});
+
+DTO_data_empleado.use( async (req,res, next)=>{
+    try {
+        let data = plainToClass(DTO("empleado").class, req.body);
+        await validate(data);
+        req.body = JSON.parse(JSON.stringify(data));
+        req.data = undefined;
+        next();
+
+    } catch (error) {
+        res.status(error.status).send(error);
+    }
+})
+
+export { middle_Verify_empleado, DTO_data_empleado } 
--- backend/routers/empleado.js
+++ backend/routers/empleado.js
@@ -1,14 +1,11 @@
 import { Router } from "express";
 import { confiGET } from "../limit/limit.js";
-
-import { 
-    get_empleados,
-    get_empleados_cargo
-} from "../controllers/empleado.js";
+import { middle_Verify_empleado, DTO_data_empleado } from "../middleware/empleado.js";
+import { get_empleados, get_empleados_cargo } from "../controllers/empleado.js";
 
 const empleados = Router();
 
 empleados.get('/', confiGET(), get_empleados);
-empleados.get('/cargo=:cg', confiGET(), get_empleados_cargo);
+empleados.get('/cargo=:cg', confiGET(), get_empleados_cargo, middle_Verify_empleado);
 
 export default empleados;
