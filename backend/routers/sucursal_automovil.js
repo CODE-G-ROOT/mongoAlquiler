810c599a4a2834cd239246ce1239e5b81141d03a
JuanDavidAvilaRaveloCampus <juandavidavilaravelo856@gmail.com>
2023-08-16 13:28:44 -0500

    feat: :safety_vest: Middleware y DTO
    Implementación del middleware y dto para seguridad y validación

--- backend/routers/sucursal_automovil.js
+++ backend/routers/sucursal_automovil.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { confiGET } from "../limit/limit.js";
 import { get_sucursal_automovil } from "../controllers/sucursal_automovil.js";
+import { middle_Verify_sucursal_automovil, DTO_data_sucursal_automovil } from "../middleware/sucursal_automovil.js";
 
 const sucursal_automovil = Router();
 
-sucursal_automovil.get('/', confiGET(), get_sucursal_automovil)
+sucursal_automovil.get('/', confiGET(), get_sucursal_automovil, middle_Verify_sucursal_automovil)
 
 export default sucursal_automovil;
